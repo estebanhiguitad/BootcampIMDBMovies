30fcdf643670d1cd36de18756ca8239c2b63d1ae
Esteban Higuita <esteban.higuita@ceiba.com.co>
2022-06-06 16:30:19 -0500

    Crear plantilla de proyecto

---
+++ app/src/main/java/co/com/bootcamp/imdb/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.com.bootcamp.imdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
