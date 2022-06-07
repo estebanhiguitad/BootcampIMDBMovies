8d7733a8a8cbdb40caef51bd0a24100e38f197ec
Esteban Higuita <esteban.higuita@ceiba.com.co>
2022-06-07 11:03:30 -0500

    Crear paquetes para representación de arquitectura

--- app/src/main/java/co/com/bootcamp/imdb/userinterface/MainActivity.kt
+++ app/src/main/java/co/com/bootcamp/imdb/userinterface/MainActivity.kt
@@ -1,7 +1,8 @@
-package co.com.bootcamp.imdb
+package co.com.bootcamp.imdb.userinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.com.bootcamp.imdb.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
