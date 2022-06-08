182ca4b01f421d4ea0b8aa580e031972fba2b42d
Zorayda Gutiérrez Montes <zorayda.gutierrez@ceiba.com.co>
2022-06-08 00:02:05 -0500

    Implementación de login interceptor para imprimir respuestas y creación del manager para retrofit

--- app/src/main/java/co/com/bootcamp/imdb/data/source/RetrofitManager.kt
+++ app/src/main/java/co/com/bootcamp/imdb/data/source/RetrofitManager.kt
@@ -1,4 +1,30 @@
 package co.com.bootcamp.imdb.data.source
 
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 class RetrofitManager {
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+    private val gson = GsonBuilder()
+        .setLenient()
+        .create()
+
+    fun create(url:String): RetrofitExampleDataSource {
+        val builder = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+        val mRetrofit = builder.build()
+        return mRetrofit.create(RetrofitExampleDataSource::class.java)
+    }
 }
