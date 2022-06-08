5eebe3198fc7ae2f1af15cbc0e3fc76d971a6e40
Zorayda Gutiérrez Montes <zorayda.gutierrez@ceiba.com.co>
2022-06-08 00:30:27 -0500

    Creación del endpoint

--- app/src/main/java/co/com/bootcamp/imdb/data/source/RetrofitExampleDataSource.kt
+++ app/src/main/java/co/com/bootcamp/imdb/data/source/RetrofitExampleDataSource.kt
@@ -1,5 +1,16 @@
 package co.com.bootcamp.imdb.data.source
 
+import co.com.bootcamp.imdb.domain.models.MovieExampleModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface RetrofitExampleDataSource {
-    suspend fun getMovies()
+
+    @GET("list/{id}")
+    suspend fun getMoviesList(
+        @Path("id") listId : Int?,
+        @Query("api_key") apiKey: String?
+    ): Call<MovieExampleModel?>?
 }
